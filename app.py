862d47aa42e9557ee6b124e240b54cbdfa765a90
Ellen Houghton <ellen.a.ash@gmail.com>
2023-11-05 11:27:49 +0000

    ai connection added

--- app.py
+++ app.py
@@ -1,14 +1,21 @@
 from flask import Flask, request, jsonify
 from Summarize import Summarize
+from summarize_ai import summarize_with_ai
 
 app = Flask(__name__)
+
+# AI_key = False
+AI_key = True
 
 @app.route('/slapi/summarize', methods=['GET', 'POST'])
 def summarize():
 	if request.method == 'POST':
 		data = request.json
 	
-		summary = Summarize.create_summary(data)
+		if AI_key == True:
+			summary = summarize_with_ai(data).choices[0].text
+		else:
+			summary = Summarize.create_summary(data)
 
 		return jsonify(summary)
 	
@@ -16,5 +23,5 @@ def summarize():
 		return jsonify({"error": "No data to summarize"})
 
 if __name__ == '__main__':
-	# app.run(port=5500, debug=True)
-	app.run()
+	app.run(port=5500, debug=True)
+	# app.run()
